e5e58df756d8f80718019b66dd22515eb11cd39e
K3A <root@k3a.me>
2015-03-29 21:09:05 +0200

    initial commit

---
+++ fixphp.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# PHP file tag fixer by K3A.me
+# Changes <? to <?php in each file
+
+abort("Usage: fixphp.rb <path_to_a_folder>") if ARGV.length == 0
+
+Dir[ARGV[0]+"/**/*"].each do |fn|
+    next if File.directory?(fn)
+
+    puts " * #{fn}"
+
+    data = File.read(fn).gsub!(/<\?\s/, "<?php ")
+
+    File.open(fn, 'w') { |f| f << data }
+end
